32b8a1fb2e25c4dd949c7ee325269c87e62a5688
Ten-Young Guh <tenyoung795@gmail.com>
2016-02-23 14:15:14 -0500

    Make test-index-complexity concurrent

--- test-index-complexity.cpp
+++ test-index-complexity.cpp
@@ -1,10 +1,13 @@
 #include <cmath>
 #include <cstddef>
 
+#include <algorithm>
 #include <chrono>
+#include <future>
 #include <iostream>
 #include <random>
 #include <ratio>
+#include <vector>
 
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics/mean.hpp>
@@ -21,9 +24,14 @@ using namespace boost::accumulators;
 
 using Duration = duration<double, steady_clock::period>;
 
-Duration mean_duration(std::size_t size) {
+constexpr std::size_t STRIDE_LENGTH = 8192;
+
+Duration mean_duration(std::size_t num_strides) {
+    auto size = num_strides * STRIDE_LENGTH;
+
     cs540::Map<std::size_t, volatile bool> map;
     std::vector<std::size_t> lookup;
+    lookup.reserve(size);
     for (std::size_t i = 0; i < size; ++i) {
         map.insert({i, false});
         lookup.push_back(i);
@@ -31,12 +39,24 @@ Duration mean_duration(std::size_t size) {
     std::shuffle(lookup.begin(), lookup.end(),
                  std::default_random_engine {std::random_device{} ()});
 
+    std::vector<std::future<Duration>> futures;
+    futures.reserve(num_strides);
+    for (std::size_t i = 0; i < num_strides; ++i) {
+        futures.push_back(std::async([&map, &lookup, i] {
+            accumulator_set<double, stats<tag::mean>> durations;
+             for (std::size_t j = 0; j < STRIDE_LENGTH; ++j) {
+                auto start = steady_clock::now();
+                map.index(lookup[i * STRIDE_LENGTH + j])->second = true;
+                auto duration = steady_clock::now() - start;
+                durations(duration.count());
+            }
+            return Duration {mean(durations)};
+        }));
+    }
+
     accumulator_set<double, stats<tag::mean>> durations;
-    for (auto i : lookup) {
-        auto start = steady_clock::now();
-        map.index(i)->second = true;
-        auto duration = steady_clock::now() - start;
-        durations(duration.count());
+    for (auto &future : futures) {
+        durations(future.get().count());
     }
     return Duration {mean(durations)};
 }
@@ -52,15 +72,23 @@ auto correlation(std::size_t n, Xs &&xs, Ys &&ys, XYs &&xys) {
 
 int main() {
     constexpr std::size_t N = 4;
+    std::vector<std::future<Duration>> futures;
+    futures.reserve(N);
     accumulator_set<double, stats<tag::mean, tag::variance>> ns, lg_ns, ks;
     accumulator_set<double, stats<tag::sum>> k_ns, k_lg_ns;
     for (std::size_t i = 0; i < N; ++i) {
-        auto n = 8192 << i;
-        std::cout << n << " elements\n";
+        std::size_t num_strides = 1 << i;
+        futures.push_back(std::async(mean_duration, num_strides));
+    }
+    for (std::size_t i = 0; i < N; ++i) {
+        std::size_t num_strides = 1 << i;
+        auto n = num_strides * STRIDE_LENGTH;
         ns(n);
         auto lg_n = std::log2(n);
         lg_ns(lg_n);
-        auto k = mean_duration(n);
+
+        std::cout << n << " elements:\n";
+        auto k = futures[i].get();
         auto us = std::chrono::duration_cast<duration<double, std::micro>>(k);
         std::cout << '\t' << us.count() << " μs\n";
         ks(k.count());
